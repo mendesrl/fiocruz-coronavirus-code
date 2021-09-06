98b1481426341506338062eaea5df544bb62fa24
mendesrl <ribeiro.larissamendes@gmail.com>
2021-09-05 20:57:47 -0400

    removendo comentarios e adicionando o font family

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Header />
     <Body />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
 import Header from './components/Header.vue';
 import Body from './components/Body.vue';
 
@@ -30,7 +27,7 @@ export default {
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto,Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
